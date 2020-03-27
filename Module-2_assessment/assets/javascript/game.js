1d7b0a4950b9de70586cd47c762342fefc52e023
Ben B <bbarber314@gmail.com>
2020-03-27 17:45:42 -0500

    Commented game.js code to make it more readable

--- Module-2_assessment/assets/javascript/game.js
+++ Module-2_assessment/assets/javascript/game.js
@@ -1,21 +1,21 @@
-let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-let possibleWords = ["doctor","dalek","cyberman","tardis","companion","rose","martha","donna","amy","rory","pond","clara","bill","nardole","river","song","regeneration"];
+let alphabet = "abcdefghijklmnopqrstuvwxyz".split(""); //list of the alphabet, to check if the key pressed is a letter
+let possibleWords = ["doctor","dalek","cyberman","tardis","companion","rose","martha","donna","amy","rory","pond","clara","bill","nardole","river","song","regeneration"]; //my themed words
 const gameState = {
-    word : "doctor",
-    wordToBeGuessed : [],
-    correctGuesses : [],
-    wins : 0,
-    games : 0,
-    incorrectGuesses : [],
-    guessesRemaining : 13,
+    word : "doctor", //just supplying a default word here, in case it fails to initialize somehow
+    wordToBeGuessed : [], //array holding the correct answer
+    correctGuesses : [], //array holding the current guesses
+    wins : 0, //number of wins by the user
+    games : 0, //number of games played by the user
+    incorrectGuesses : [], //array holding the incorrect guesses of the user
+    guessesRemaining : 13, //total number of wrong guesses allowed
 
-    pickWord : function(listOfWords){
+    pickWord : function(listOfWords){ //This function does what's on the sticker: it takes a random word from the possibleWords list, and saves it to gameState word
         let randomNumber = Math.floor(Math.random()*listOfWords.length);
         console.log(randomNumber);
         this.word = listOfWords[randomNumber];
     },
 
-    makeBlank : function(){
+    makeBlank : function(){ //This populates the wordToBeGuessed array based on the current word and it populates the correctGuesses array based on the length of the wordToBeGuessed array
         this.wordToBeGuessed=this.word.split("");
         let blank = [];
         for (i=0; i<this.wordToBeGuessed.length; i++) {
@@ -24,18 +24,17 @@ const gameState = {
         this.correctGuesses = blank;
     },
 
-    updateDisplay : function(){
+    updateDisplay : function(){//This one updates the commonly occurring fields on the HTML page: what the current guess is, how many guesses are left, and what the wrong guesses were
         let correctGuessesH3 = document.querySelector('#wordToBeGuessed');
-        let winsH3 = document.querySelector('#wins');
         let guessesLeftH3 = document.querySelector('#guessesLeft');
         let wrongGuessesH3 = document.querySelector('#wrongGuesses');
 
-        let currentGuess = ""
+        let currentGuess = "" //make the string of the current state of the guess
         for (i=0;i<this.correctGuesses.length;i++){
             currentGuess += this.correctGuesses[i];
         }
 
-        let wrongGuess = ""
+        let wrongGuess = "" //make a string of the current state of incorrect guesses. Be clever about it so the last letter guessed doesn't have a trailing comma
         if(this.incorrectGuesses.length>1){
             for (i=0;i<this.incorrectGuesses.length-1;i++){
                 wrongGuess += this.incorrectGuesses[i]+", ";
@@ -45,10 +44,15 @@ const gameState = {
             wrongGuess +=this.incorrectGuesses[0];
         }
 
+        //populate all the H3 headers! All of them! (except the ones that only changes on win/loss)
         correctGuessesH3.innerText = currentGuess;
         guessesLeftH3.innerText = this.guessesRemaining;
         wrongGuessesH3.innerText = wrongGuess;
+    },
 
+    winUpdate : function(){ //updates the win and win percentage field
+        let winsH3 = document.querySelector('#wins');
+        
         if(this.games>0){
             winsH3.innerText = `${this.wins} (${(100*this.wins/this.games).toFixed(2)}%)`;
         }else{
@@ -56,8 +60,9 @@ const gameState = {
         }
     },
 
-    reset : function (){
+    reset : function (){// This resets a lot of the gameState back to it's initial state. We pick a new word, update the win record, reset the incorrectGuesses array, reset the number of guessesRemaining, and then re-generate the wordToBeGuessed and correctGuesses arrays
         this.games += 1;
+        this.winUpdate();
         this.pickWord(possibleWords);
         this.incorrectGuesses = [];
         this.guessesRemaining = 13;
@@ -65,7 +70,7 @@ const gameState = {
         this.updateDisplay();
     },
 
-    checkState : function(){
+    checkState : function(){//Check to see if the game is over, either because the correctGuesses array is out of blanks or the remainingGuesses has hit 0
         console.log(`Checking State`)
         let gameStateH3 = document.querySelector('#gameState');
         if (this.correctGuesses.includes("_ ") === false){
@@ -78,11 +83,11 @@ const gameState = {
         }
     },
     
-    guess : function(event){
+    guess : function(event){ //The workhorse: on keystroke, check if a letter. If a letter, check if it's in the wordToBeGuessed array, and if it is, check where in the array it is, and replace those blanks with the keystroke. If a letter and not in the wordToBeGuessed, check if it's in the incorrectGuesses array. If it is, do nothing, it it isn't, add it to that array, and subtract a guess. If it's not a letter, ignore it.
         const key = event.key;
         if(alphabet.includes(key)) {
-            console.log(`You pressed ${key}`);
-            console.log(gameState.wordToBeGuessed.includes(key));
+            console.log(`You pressed ${key}`);//console debug
+            console.log(`Is ${key} in the word being guessed?: ${gameState.wordToBeGuessed.includes(key)}`);//console debug
             if (gameState.wordToBeGuessed.includes(key)){
                 let positions = [];
                 for(i=0;i<gameState.wordToBeGuessed.length;i++){
@@ -93,17 +98,18 @@ const gameState = {
                 for(i=0;i<positions.length;i++){
                     gameState.correctGuesses[positions[i]] = gameState.wordToBeGuessed[positions[i]];
                 }
-                console.log(positions)
+                console.log(`The letter ${key} is at positions ${positions}.`)// Console debug
             } else {
                 if(gameState.incorrectGuesses.includes(key)){
-                    console.log(`You've already guessed ${key}`)
+                    console.log(`You've already guessed ${key}`)// Console debug
                 } else {
                     gameState.incorrectGuesses.push(key);
                     gameState.guessesRemaining -=1;
+                    console.log(`${key} has been added to the incorrect guesses. Guesses Remaining: ${gameState.guessesRemaining}`);//console debug
                 }
             }
         } else {
-            console.log(`That's not a letter!`);
+            console.log(`That's not a letter!`);//console debug;
         }
         gameState.updateDisplay();
         gameState.checkState();
@@ -111,6 +117,7 @@ const gameState = {
 }
 
 gameState.pickWord(possibleWords);
+gameState.winUpdate();
 gameState.makeBlank();
 gameState.updateDisplay();
 
