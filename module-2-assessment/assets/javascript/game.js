a89f063a5d2ddde5931e0a999e79ef16cb3ccb48
Ben B <bbarber314@gmail.com>
2020-03-27 15:53:45 -0500

    Wrong letter + update to how incorrect guesses are rendered to deal with trailing comma

--- module-2-assessment/assets/javascript/game.js
+++ module-2-assessment/assets/javascript/game.js
@@ -27,10 +27,13 @@ const gameState = {
         }
 
         let wrongGuess = ""
-        if(this.incorrectGuesses.length>0){
-            for (i=0;i<this.incorrectGuesses.length;i++){
-                wrongGuess += this.incorrectGuesses[i];
+        if(this.incorrectGuesses.length>1){
+            for (i=0;i<this.incorrectGuesses.length-1;i++){
+                wrongGuess += this.incorrectGuesses[i]+", ";
             }
+            wrongGuess +=this.incorrectGuesses[this.incorrectGuesses.length-1]
+        } else if (this.incorrectGuesses.length===1){
+            wrongGuess +=this.incorrectGuesses[0];
         }
 
         correctGuessesH3.innerText = currentGuess;
@@ -47,8 +50,22 @@ const gameState = {
     guess : function(event){
         const key = event.key;
         if(alphabet.includes(key)) {
-            alert(`You pressed the letter ${key}`);
+            console.log(`You pressed ${key}`);
+            console.log(gameState.wordToBeGuessed.includes(key));
+            if (gameState.wordToBeGuessed.includes(key)){
+                console.log(gameState.wordToBeGuessed.indexOf(key))
+            } else {
+                if(gameState.incorrectGuesses.includes(key)){
+                    console.log(`You've already guessed ${key}`)
+                } else {
+                    gameState.incorrectGuesses.push(key);
+                    gameState.guessesRemaining -=1;
+                }
+            }
+        } else {
+            console.log(`That's not a letter!`);
         }
+        gameState.updateDisplay();
     }
 }
 
